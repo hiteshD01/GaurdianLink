c7f7eec2d8822d2a506a0f1e6f39f399f253e195
hiteshD01 <hitesh.crestcoder@gmail.com>
2024-07-05 18:08:08 +0530

    del: remove firebaseService

--- firebaseService.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./gaurdianlink-c8cf0-firebase-adminsdk-wqeyn-c46e7c9e70.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
