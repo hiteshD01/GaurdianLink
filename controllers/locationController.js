67f71da0232bc86438b329025d33168592fde46c
hiteshD01 <hitesh.crestcoder@gmail.com>
2024-09-24 18:11:14 +0530

    fix : roll back gethotspot

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -335,134 +335,137 @@ exports.getRecentSosLocations = async (req, res) => {
 };
 
 exports.getHotspots = async (req, res) => {
-  const { type, start_date, end_date, company_id } = req.query;
-  const today = moment().startOf("day");
-  const endOfToday = moment().endOf("day");
-  let dateRange;
-
-  switch (type) {
-    case "today":
-      dateRange = { $gte: today.toDate(), $lte: endOfToday.toDate() };
-      break;
-    case "yesterday":
-      const yesterday = today.subtract(1, "days");
-      dateRange = {
-        $gte: yesterday.startOf("day").toDate(),
-        $lte: yesterday.endOf("day").toDate(),
-      };
-      break;
-    case "thisWeek":
-      dateRange = {
-        $gte: today.startOf("week").toDate(),
-        $lte: endOfToday.toDate(),
-      };
-      break;
-    case "thisMonth":
-      dateRange = {
-        $gte: today.startOf("month").toDate(),
-        $lte: endOfToday.toDate(),
-      };
-      break;
-    case "thisYear":
-      dateRange = {
-        $gte: today.startOf("year").toDate(),
-        $lte: endOfToday.toDate(),
-      };
-      break;
-    default:
-      dateRange = null;
-  }
-
-  try {
-    let matchCriteria = { type: "sos" };
-
-    // Add date range filtering if start_date and end_date are provided
-    if (start_date && end_date) {
-      matchCriteria.createdAt = {
-        $gte: new Date(start_date),
-        $lte: new Date(end_date),
-      };
-    } else if (dateRange) {
-      matchCriteria.createdAt = dateRange;
-    }
-
-    const hotspots = await Location.aggregate([
-      { $match: matchCriteria },
-      {
-        // Join with the User collection to get the company_id
-        $lookup: {
-          from: "users", // The User collection
-          localField: "user_id", // user_id in the Location collection
-          foreignField: "_id", // _id in the User collection
-          as: "user", // Result will be stored in the 'user' field
-        },
-      },
-      {
-        $unwind: "$user", // Unwind the joined user array
-      },
-      {
-        // Match based on the provided company_id
-        $match: {
-          "user.company_id": company_id,
-        },
-      },
-      {
-        $project: {
-          lat: {
-            $convert: {
-              input: "$lat",
-              to: "double",
-              onError: null,  // Handle conversion errors
-              onNull: null,   // Handle null values
-            },
-          },
-          long: {
-            $convert: {
-              input: "$long",
-              to: "double",
-              onError: null,
-              onNull: null,
-            },
-          },
-          address: 1,
-          createdAt: 1,
-        },
-      },
-      {
-        $project: {
-          lat: { $round: ["$lat", 4] },  // Round lat to 4 decimal places
-          long: { $round: ["$long", 4] },  // Round long to 4 decimal places
-          address: 1,
-        },
-      },
-      {
-        $group: {
-          _id: { lat: "$lat", long: "$long" }, // Group by lat and long
-          address: { $first: "$address" },     // Take the first address from grouped records
-          count: { $sum: 1 },                  // Count the occurrences of each unique lat/long pair
-        },
-      },
-      {
-        $sort: { count: -1 },  // Sort by the number of times the hotspot has been called
-      },
-    ]);
-
-    if (hotspots.length === 0) {
-      return res.status(200).json([]);
-    }
-
-    const hotspotsWithCount = hotspots.map((hotspot) => ({
-      lat: hotspot._id.lat,
-      long: hotspot._id.long,
-      address: hotspot.address,  // Use the first address from the group
-      timesCalled: hotspot.count,
-    }));
-
-    res.status(200).json(hotspotsWithCount);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
 };
+
+// exports.getHotspots = async (req, res) => {
+//   const { type, start_date, end_date, company_id } = req.query;
+//   const today = moment().startOf("day");
+//   const endOfToday = moment().endOf("day");
+//   let dateRange;
+
+//   switch (type) {
+//     case "today":
+//       dateRange = { $gte: today.toDate(), $lte: endOfToday.toDate() };
+//       break;
+//     case "yesterday":
+//       const yesterday = today.subtract(1, "days");
+//       dateRange = {
+//         $gte: yesterday.startOf("day").toDate(),
+//         $lte: yesterday.endOf("day").toDate(),
+//       };
+//       break;
+//     case "thisWeek":
+//       dateRange = {
+//         $gte: today.startOf("week").toDate(),
+//         $lte: endOfToday.toDate(),
+//       };
+//       break;
+//     case "thisMonth":
+//       dateRange = {
+//         $gte: today.startOf("month").toDate(),
+//         $lte: endOfToday.toDate(),
+//       };
+//       break;
+//     case "thisYear":
+//       dateRange = {
+//         $gte: today.startOf("year").toDate(),
+//         $lte: endOfToday.toDate(),
+//       };
+//       break;
+//     default:
+//       dateRange = null;
+//   }
+
+//   try {
+//     let matchCriteria = { type: "sos" };
+
+//     // Add date range filtering if start_date and end_date are provided
+//     if (start_date && end_date) {
+//       matchCriteria.createdAt = {
+//         $gte: new Date(start_date),
+//         $lte: new Date(end_date),
+//       };
+//     } else if (dateRange) {
+//       matchCriteria.createdAt = dateRange;
+//     }
+
+//     const hotspots = await Location.aggregate([
+//       { $match: matchCriteria },
+//       {
+//         // Join with the User collection to get the company_id
+//         $lookup: {
+//           from: "users", // The User collection
+//           localField: "user_id", // user_id in the Location collection
+//           foreignField: "_id", // _id in the User collection
+//           as: "user", // Result will be stored in the 'user' field
+//         },
+//       },
+//       {
+//         $unwind: "$user", // Unwind the joined user array
+//       },
+//       {
+//         // Match based on the provided company_id
+//         $match: {
+//           "user.company_id": company_id,
+//         },
+//       },
+//       {
+//         $project: {
+//           lat: {
+//             $convert: {
+//               input: "$lat",
+//               to: "double",
+//               onError: null,  // Handle conversion errors
+//               onNull: null,   // Handle null values
+//             },
+//           },
+//           long: {
+//             $convert: {
+//               input: "$long",
+//               to: "double",
+//               onError: null,
+//               onNull: null,
+//             },
+//           },
+//           address: 1,
+//           createdAt: 1,
+//         },
+//       },
+//       {
+//         $project: {
+//           lat: { $round: ["$lat", 4] },  // Round lat to 4 decimal places
+//           long: { $round: ["$long", 4] },  // Round long to 4 decimal places
+//           address: 1,
+//         },
+//       },
+//       {
+//         $group: {
+//           _id: { lat: "$lat", long: "$long" }, // Group by lat and long
+//           address: { $first: "$address" },     // Take the first address from grouped records
+//           count: { $sum: 1 },                  // Count the occurrences of each unique lat/long pair
+//         },
+//       },
+//       {
+//         $sort: { count: -1 },  // Sort by the number of times the hotspot has been called
+//       },
+//     ]);
+
+//     if (hotspots.length === 0) {
+//       return res.status(200).json([]);
+//     }
+
+//     const hotspotsWithCount = hotspots.map((hotspot) => ({
+//       lat: hotspot._id.lat,
+//       long: hotspot._id.long,
+//       address: hotspot.address,  // Use the first address from the group
+//       timesCalled: hotspot.count,
+//     }));
+
+//     res.status(200).json(hotspotsWithCount);
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// };
 
 
 // exports.getHotspots = async (req, res) => {
